3bea0018c23f8a9e2f1004f0adc449a59e07fe4a
Xu Mitcuha <1205724055@qq.com>
2020-08-05 18:53:26 +0800

    8.5

---
+++ src/indexPage.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    let canvasHeight = window.innerHeight;
+    let canvasWidth = window.innerWidth;
+    console.log(canvasHeight);
+    console.log(canvasWidth);
+})
